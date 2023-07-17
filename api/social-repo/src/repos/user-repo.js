d8f065bce339321de43ec898cfbcde0baa1f230c
Ryan Moore <mooreaz92@gmail.com>
2023-07-16 23:24:41 -0500

    Updating for section 35

---
+++ api/social-repo/src/repos/user-repo.js
@@ -0,0 +1,35 @@
+const pool = require('../pool');
+const toCamelCase = require('./utils/to-camel-case');
+
+class UserRepo {
+  static async find() {
+    const { rows } = await pool.query('SELECT * FROM users;');
+
+    return toCamelCase(rows);
+  }
+
+  static async findById(id) {
+    const { rows } = await pool.query('SELECT * FROM users WHERE id = $1;', [
+      id,
+    ]);
+
+    return toCamelCase(rows)[0];
+  }
+
+  static async insert(username, bio) {
+    const {
+      rows,
+    } = await pool.query(
+      'INSERT INTO users (username, bio) VALUES ($1, $2) RETURNING *;',
+      [username, bio]
+    );
+
+    return toCamelCase(rows)[0];
+  }
+
+  static async update() {}
+
+  static async delete() {}
+}
+
+module.exports = UserRepo;
--- api/social-repo/src/routes/users.js
+++ api/social-repo/src/routes/users.js
@@ -1,12 +1,36 @@
 const express = require('express');
+const UserRepo = require('../repos/user-repo');
 
 const router = express.Router();
 
-router.get('/users', async (req, res) => {});
+router.get('/users', async (req, res) => {
+  // Run a query to get all users
+  const users = await UserRepo.find();
 
-router.get('/users/:id', async (req, res) => {});
+  // Send the result back to the person
+  // who made this request
+  res.send(users);
+});
 
-router.post('/users', async (req, res) => {});
+router.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const user = await UserRepo.findById(id);
+
+  if (user) {
+    res.send(user);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post('/users', async (req, res) => {
+  const { username, bio } = req.body;
+
+  const user = await UserRepo.insert(username, bio);
+
+  res.send(user);
+});
 
 router.put('/users/:id', async (req, res) => {});
 
